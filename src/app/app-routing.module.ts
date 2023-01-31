51c5e7118d3e130c5d19108df7efb3aebc8a6fb2
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-31 15:34:09 -0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ClassroomComponent} from "./classroom/component/classroom/classroom.component";
 
 const routes: Routes = [{
   path: '',
   loadChildren: () => import('../app/main-page/main-page.module').then(m=>m.MainPageModule)
-},
-  {
-    path: 'classroom',
-//    loadChildren: () => import('../app/classroom/classroom.module').then(m=>m.ClassroomModule)
-    component: ClassroomComponent
-  }
+}
 ];
 
 @NgModule({
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -5,6 +5,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {WelcomeComponent} from "../welcome/component/welcome/welcome.component";
 import {LoginComponent} from "../login/component/login/login.component";
 import {SignUpComponent} from "../sign-up/component/sign-up/sign-up.component";
+import {ClassroomComponent} from "../classroom/component/classroom/classroom.component";
 
 const routes: Routes = [{
   path: '',
@@ -21,6 +22,10 @@ const routes: Routes = [{
     {
       path: 'sign-up',
       component: SignUpComponent
+    },
+    {
+      path: 'classroom',
+      component: ClassroomComponent
     }
   ]
 }];
