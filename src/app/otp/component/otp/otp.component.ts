2db4a8294af1dc17a90c47329272f47336fcc537
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-31 20:39:00 -0100

    update

---
+++ src/app/otp/component/otp/otp.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {User} from "../../../interfaces/user.interface";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-otp',
+  templateUrl: './otp.component.html',
+  styleUrls: ['./otp.component.scss']
+})
+export class OtpComponent {
+
+  form = this.fb.group({
+    otp: ['', Validators.required],
+  })
+  data!: any;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  }
+
+  submit() {
+    const otp = this.form.controls['otp'].value;
+    console.log("-->", this.form.controls['otp'].value)
+    if(otp) {
+      this.data = {
+        otp: otp
+      }
+    }
+    console.log("payload", this.data)
+    this.authService.signup(this.data).subscribe(data => {
+      this.router.navigate(['classroom'])
+    })
+  }
+
+}
