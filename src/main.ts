2253ddcf1b87eecda106f047fb2555526c0f0dbf
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-04-05 16:40:50 +0000

    first standalone component refactor routing works

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,15 @@ import {AuthInterceptor} from "./app/interceptors/auth.interceptor";
 import {DefaultDataServiceConfig, EntityDataModule} from "@ngrx/data";
 import {environment} from "./environments/environment";
 import {provideRouter, RouterModule, Routes} from "@angular/router";
-import {importProvidersFrom} from "@angular/core";
+import {importProvidersFrom, isDevMode} from "@angular/core";
 import {StoreModule} from "@ngrx/store";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {EffectsModule} from "@ngrx/effects";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {entityConfig} from "./app/ngrx-redux/entity-metadata";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 
 const routes: Routes = [{
@@ -31,6 +34,16 @@ const routes: Routes = [{
 bootstrapApplication(AppComponent,{
   providers: [
     importProvidersFrom(
+      MatTooltipModule,
+      MatButtonModule,
+      MatDialogModule,
+      StoreDevtoolsModule.instrument({
+        maxAge: 25, // Retains last 25 states
+        logOnly: !isDevMode(), // Restrict extension to log-only mode
+        autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+        trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      }),
       HttpClientModule,
       EntityDataModule.forRoot(entityConfig),
       EffectsModule.forRoot([]),
