e14da37f2c15b86a3c20db4e6ef2517f6f82cecb
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-04-21 16:57:00 +0000

    pdf works

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import { Observable } from 'rxjs';
 import { DashboardService } from 'src/app/services/dashboard.service';
 
@@ -12,13 +14,54 @@ import { DashboardService } from 'src/app/services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   usersNumber$ = new Observable();
-
   pieChartData$ = new Observable();
+  publishedClassesTablePdf: any[] = [];
+  totalUsersClassesTablePdf: any[] = [];
 
   constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {
-    this.usersNumber$ = this.dashboardService.getUsersAndClasses();
     this.pieChartData$ = this.dashboardService.getPublishedClasses();
+    this.usersNumber$ = this.dashboardService.getUsersAndClasses();
+    this.getPdfData();
+  }
+
+  async getPdfData() {
+    await this.dashboardService.getUsersAndClasses().subscribe((data: any) => {
+      data.forEach((item: any) => {
+        this.totalUsersClassesTablePdf.push([item.name, item.value]);
+      });
+    });
+
+    await this.dashboardService.getPublishedClasses().subscribe((data: any) => {
+      data.forEach((item: any) => {
+        this.publishedClassesTablePdf.push([item.name, item.value]);
+      });
+    });
+  }
+
+  exportPdf() {
+    const doc = new jsPDF();
+
+    doc.addImage(
+      '../../../assets/images/BRING_apostrophe_red_pos_rgb.png',
+      'PNG',
+      80,
+      150,
+      30,
+      30
+    );
+
+    autoTable(doc, {
+      head: [['Stats', 'Number']],
+      body: this.totalUsersClassesTablePdf,
+    });
+
+    autoTable(doc, {
+      head: [['Users', 'Classes Published']],
+      body: this.publishedClassesTablePdf,
+    });
+
+    doc.save('table.pdf');
   }
 }
